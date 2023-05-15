90807735baf09f9f709b6ef34ded84a78d1de242
David Nevarez <david.nevarez03@hotmail.com>
2023-05-15 03:41:46 -0700

    completed assignment

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,16 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(num1: i32, num2: i32) -> i32 {
+  num1 + num2
+}
+
+fn display_int(num: i32) {
+  println!("{:?}", num);
+}
+
+fn main() {
+  let total = add(10, 10);
+
+  display_int(total);
+}
